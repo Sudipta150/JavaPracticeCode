0c27844ebfe3c88b6644cf88ca7aa039f0210372
Sudipta150 <78503291+Sudipta150@users.noreply.github.com>
2025-07-01 19:19:08 +0530

    Create CeillingNumber.java
    Find the samllest element in array which is <= target

---
+++ CeillingNumber.java
@@ -0,0 +1,41 @@
+public class CeillingNumber {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+		int target = 15;
+
+		int result = cellingNumber(arr, target);
+		System.out.println(result);
+
+	}
+
+	public static int cellingNumber(int[] arr, int target) {
+		
+		int start =0;
+		int end =arr.length-1;
+		
+		while(start<= end) {
+			
+			int mid = start + (end -start)/2;
+			
+			if(target>arr[mid]) {
+				
+				start=mid+1;
+			}
+			
+			else if(target<arr[mid]) {
+				
+				end=mid-1;
+			}
+			
+			else if(target == arr[mid]) {
+				
+				return arr[mid];
+			}
+		}
+		
+		return arr[start];
+	}
+
+}
