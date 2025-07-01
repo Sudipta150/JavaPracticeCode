e52c6845d8859b0acbb509f0e8adce83d34dc145
Sudipta150 <78503291+Sudipta150@users.noreply.github.com>
2025-07-01 20:17:41 +0530

    Create Find SmallestLetterGreaterThaTarget.java

---
+++ Find SmallestLetterGreaterThaTarget.java
@@ -0,0 +1,29 @@
+class Solution {
+   //https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+public char nextGreatestLetter(char[] letters, char target) {
+        
+
+        int start =0;
+		int end =letters.length-1;
+		
+		while(start<= end) {
+			
+			int mid = start + (end -start)/2;
+			
+			if(target>=letters[mid]) {
+				
+				start=mid+1;
+			}
+			
+			else  {
+				
+				end=mid-1;
+			}
+			
+		
+		}
+		
+		return letters[start%letters.length];
+    }
+}
